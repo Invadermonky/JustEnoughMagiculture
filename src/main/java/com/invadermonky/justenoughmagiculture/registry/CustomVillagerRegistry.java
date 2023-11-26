43e018f16c21cb557f55cbb7199339555f4a96c7
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2023-11-25 21:44:02 -0800

    added Harvester's Night integration

--- src/main/java/com/invadermonky/justenoughmagiculture/registry/CustomVillagerRegistry.java
+++ src/main/java/com/invadermonky/justenoughmagiculture/registry/CustomVillagerRegistry.java
@@ -1,6 +1,5 @@
 package com.invadermonky.justenoughmagiculture.registry;
 
-import com.invadermonky.justenoughmagiculture.integrations.jei.categories.villager.CustomPigtificateEntry;
 import com.invadermonky.justenoughmagiculture.integrations.jei.categories.villager.CustomVillagerEntry;
 
 import java.util.LinkedList;
@@ -10,7 +9,6 @@ public class CustomVillagerRegistry {
     private static CustomVillagerRegistry instance;
 
     private List<CustomVillagerEntry> villagers;
-    private List<CustomPigtificateEntry> pigtificates;
 
     public static CustomVillagerRegistry getInstance() {
         return instance != null ? instance : (instance = new CustomVillagerRegistry());
@@ -18,27 +16,17 @@ public class CustomVillagerRegistry {
 
     private CustomVillagerRegistry() {
         this.villagers = new LinkedList<>();
-        this.pigtificates = new LinkedList<>();
     }
 
     public void addVillagerEntry(CustomVillagerEntry entry) {
         this.villagers.add(entry);
     }
 
-    public void addPigtificateEntry(CustomPigtificateEntry entry) {
-        this.pigtificates.add(entry);
-    }
-
     public List<CustomVillagerEntry> getVillagers() {
         return this.villagers;
     }
 
-    public List<CustomPigtificateEntry> getPigtificates() {
-        return this.pigtificates;
-    }
-
     public void clear() {
         villagers.clear();
-        pigtificates.clear();
     }
 }
