fa8c37006c93c956239e0015ac9e31d7114d1314
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2023-11-25 21:43:33 -0800

    added Harvester's Night integration

---
+++ src/main/java/com/invadermonky/justenoughmagiculture/configs/mods/JEMConfigHarvestersNight.java
@@ -0,0 +1,22 @@
+package com.invadermonky.justenoughmagiculture.configs.mods;
+
+import com.invadermonky.justenoughmagiculture.JustEnoughMagiculture;
+import com.invadermonky.justenoughmagiculture.util.ModIds.ConstantNames;
+import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.Config.RequiresMcRestart;
+
+public class JEMConfigHarvestersNight {
+    private static final String MOD_NAME = ConstantNames.HARVESTERSNIGHT;
+    private static final String LANG_KEY = "config." + JustEnoughMagiculture.MOD_ALIAS + ":";
+
+    @RequiresMcRestart
+    @Comment("Enables JEM " + MOD_NAME + " JER mob integration.")
+    public boolean enableJERMobs = true;
+
+    @RequiresMcRestart
+    @Comment("JEM " + MOD_NAME + " Just Enough Resources settings.")
+    public JER JUST_ENOUGH_RESOURCES = new JER();
+    public static class JER {
+        public boolean enableHarvester = true;
+    }
+}
